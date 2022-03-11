09bb073891b24bd3325c7b464128051c83a07356
masontmorris <masonmorris8@gmail.com>
2022-03-10 20:28:53 -0500

    made function to pick random question

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,10 @@
-var start = $("#startBtn");
+var start = $("#start-btn");
 var countdownEl = $("#countdown");
 let time = 59;
+var questionContainer = $("#question-container");
+var usedNumbers = [];
+var questions = [1, 2, 3, 4, 5];
+var answers = [1, 2, 3, 4, 5];
 
 function startTimer() {
     if (time >= 0) {
@@ -11,8 +15,25 @@ function startTimer() {
     }
 }
 
+function questionPicker() {
+    var selector = Math.floor(Math.random() * 5);
+    if (usedNumbers.includes(selector)) {
+        return questionPicker;
+    }
+    usedNumbers.push(selector);
+    return selector;
+}
+
+function generateQuestion() {
+    var questionNumber = questionPicker();
+    console.log(questions[questionNumber]);
+    console.log(answers[questionNumber]);
+    questionContainer.append("<p>Hello!</p>");
+}
+
 start.click(function () {
     start.remove();
 
     var interval = setInterval(startTimer, 1000);
+    generateQuestion();
 });
